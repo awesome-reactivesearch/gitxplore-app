b80bbb64e749c5c64859122548663995c8a0c20e
Anshuman Verma <anshu.av97@gmail.com>
2019-06-01 22:52:46 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ReactiveBase, CategorySearch } from '@appbaseio/reactivesearch';
 
 import Header from './components/Header';
@@ -7,62 +7,46 @@ import Results from './components/Results';
 import theme from './theme';
 import './App.css';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentTopics: [],
-		};
-	}
+const App = props => {
+	const [currentTopics, setTopics] = useState([])
 
-	setTopics = (currentTopics) => {
-		this.setState({
-			currentTopics: currentTopics || [],
-		});
-	}
-
-	toggleTopic = (topic) => {
-		const { currentTopics } = this.state;
+	const toggleTopic = (topic) => {
 		const nextState = currentTopics.includes(topic)
 			? currentTopics.filter(item => item !== topic)
 			: currentTopics.concat(topic);
-		this.setState({
-			currentTopics: nextState,
-		});
+		setTopics(nextState)
 	}
 
-	render() {
-		return (
-			<section className="container">
-				<ReactiveBase
-					app="gitxplore-app"
-					credentials="4oaS4Srzi:f6966181-1eb4-443c-8e0e-b7f38e7bc316"
-					type="gitxplore-latest"
-					theme={theme}
-				>
-					<div className="flex row-reverse app-container">
-						<Header currentTopics={this.state.currentTopics} setTopics={this.setTopics} />
-						<div className="results-container">
-							<CategorySearch
-								componentId="repo"
-								filterLabel="Search"
-								dataField={['name', 'description', 'name.raw', 'fullname', 'owner', 'topics']}
-								placeholder="Search Repos"
-								iconPosition="left"
-								autosuggest={false}
-								URLParams
-								className="data-search-container results-container"
-								innerClass={{
-									input: 'search-input',
-								}}
-							/>
-							<Results currentTopics={this.state.currentTopics} toggleTopic={this.toggleTopic} />
-						</div>
+	return (
+		<section className="container">
+			<ReactiveBase
+				app="gitxplore-app"
+				credentials="4oaS4Srzi:f6966181-1eb4-443c-8e0e-b7f38e7bc316"
+				type="gitxplore-latest"
+				theme={theme}
+			>
+				<div className="flex row-reverse app-container">
+					<Header currentTopics={currentTopics} setTopics={setTopics} />
+					<div className="results-container">
+						<CategorySearch
+							componentId="repo"
+							filterLabel="Search"
+							dataField={['name', 'description', 'name.raw', 'fullname', 'owner', 'topics']}
+							placeholder="Search Repos"
+							iconPosition="left"
+							autosuggest={false}
+							URLParams
+							className="data-search-container results-container"
+							innerClass={{
+								input: 'search-input',
+							}}
+						/>
+						<Results currentTopics={currentTopics} toggleTopic={toggleTopic} />
 					</div>
-				</ReactiveBase>
-			</section>
-		);
-	}
+				</div>
+			</ReactiveBase>
+		</section>
+	);
 }
 
 export default App;
